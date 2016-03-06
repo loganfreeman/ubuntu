9f00727e8a84c1da274738856b3e281716192850
root <root@precise64.(none)>
2016-03-06 08:59:35 +0000

    all

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+function deploy() {
+ site=$1
+ if [ ! -f /etc/nginx/sites-available/${site} ]; then
+    echo "${site} not available"
+    return
+ fi
+
+ rm -f "/etc/nginx/sites-enabled/${site}"
+
+ ln -s /etc/nginx/sites-available/${site} /etc/nginx/sites-enabled/${site}
+
+ nginx -t
+
+ service nginx restart
+}
---
+++ scripts/php.sh
@@ -0,0 +1,4 @@
+apt-add-repository ppa:ondrej/php5-5.6
+apt-get update
+apt-get install php5
+apt-get install nginx php5-fpm php5-cli php5-mcrypt git
