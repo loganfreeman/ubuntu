bfd379495eff0c3ee3adb852e385fdea9acdca27
root <root@precise64.(none)>
2016-03-07 18:01:39 +0000

    all

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,4 +1,4 @@
 apt-add-repository ppa:ondrej/php5-5.6
 apt-get update
 apt-get install php5
-apt-get install nginx php5-fpm php5-cli php5-mcrypt git
+apt-get install nginx php5-fpm php5-cli php5-mcrypt
