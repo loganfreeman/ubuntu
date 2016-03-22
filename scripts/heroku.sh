230b55d94073abcf7627cfccd49cc374b65afc73
shanhong cheng <shanhong.cheng@utah.edu>
2016-03-22 16:45:19 -0600

    APP_KEY

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,5 +1,6 @@
 heroku buildpacks:set heroku/php
 git push heroku master
 heroku addons:add heroku-postgresql:hobby-dev
+heroku config:set APP_KEY=$(php artisan --no-ansi key:generate --show)
 heroku config | grep DATABASE_URL
 heroku run php artisan migrate
